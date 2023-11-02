49ce49eeafcc450158aa6f0fa8b09a1d6bc04d3c
karolbrol <134250879+karolbrol@users.noreply.github.com>
2023-11-02 07:10:57 +0100

    Add grid drawing functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const INITIAL_GRID = 16;
+
+const EASContainer = document.querySelector('#eas-container');
+
+//create initial grid
+window.addEventListener('load', makeGrid(INITIAL_GRID))
+
+function makeGrid(size) {
+
+    clearGrid();
+
+    for (let row = 0; row < size; row++) {
+        addEASRow(size);
+    }
+}
+
+function clearGrid() {
+    while (EASContainer.firstChild) {
+        EASContainer.lastChild.remove();
+    }
+}
+
+function addEASRow(size) {
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('eas-row');
+    for (let cell = 0; cell < size; cell++) {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('eas-cell');
+        rowElement.appendChild(cellElement);
+    }
+    EASContainer.appendChild(rowElement);
+}
